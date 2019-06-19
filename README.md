006677735aa89fa0c0d65aa231363d3c126bd547
root <admin@example.com>
2019-06-19 19:40:19 +0800

    提示错误

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tolower
 idea 插件 显示大写常量的小写提示
+
+https://plugins.jetbrains.com/plugin/12524-tolower
--- src/main/java/org/py/plugin/codeInspection/ToLowerInspection.java
+++ src/main/java/org/py/plugin/codeInspection/ToLowerInspection.java
@@ -1,14 +1,11 @@
 package org.py.plugin.codeInspection;
 
-import com.intellij.codeInsight.daemon.impl.HighlightInfoType;
-import com.intellij.codeInspection.*;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.project.Project;
+import com.intellij.codeInspection.AbstractBaseJavaLocalInspectionTool;
+import com.intellij.codeInspection.ProblemHighlightType;
+import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.*;
 import com.intellij.psi.impl.source.PsiFieldImpl;
-import com.intellij.psi.tree.IElementType;
 import com.intellij.ui.DocumentAdapter;
-import com.intellij.util.IncorrectOperationException;
 import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
@@ -16,10 +13,7 @@ import org.jetbrains.annotations.NotNull;
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import java.awt.*;
-import java.util.StringTokenizer;
 import java.util.regex.Pattern;
-
-import static com.siyeh.ig.psiutils.ExpressionUtils.isNullLiteral;
 
 /**
  * @author max
@@ -27,10 +21,10 @@ import static com.siyeh.ig.psiutils.ExpressionUtils.isNullLiteral;
  */
 public class ToLowerInspection extends AbstractBaseJavaLocalInspectionTool {
   // This string holds a list of classes relevant to this inspection.
-  @SuppressWarnings({"WeakerAccess"})
+
   @NonNls
-  public String CHECKED_CLASSES = "java.lang.String;java.util.Date";
-  
+  private String CHECKED_CLASSES = "java.lang.String;java.util.Date";
+
   /**
    * This method is called to get the panel describing the inspection.
    * It is called every time the user selects the inspection in preferences.
@@ -44,7 +38,8 @@ public class ToLowerInspection extends AbstractBaseJavaLocalInspectionTool {
     JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
     final JTextField checkedClasses = new JTextField(CHECKED_CLASSES);
     checkedClasses.getDocument().addDocumentListener(new DocumentAdapter() {
-      public void textChanged(DocumentEvent event) {
+      @Override
+      public void textChanged(@NotNull DocumentEvent event) {
         CHECKED_CLASSES = checkedClasses.getText();
       }
     });
@@ -52,11 +47,15 @@ public class ToLowerInspection extends AbstractBaseJavaLocalInspectionTool {
     return panel;
   }
 
-  public static boolean isNumeric(String str){
+  private static boolean isNumeric(String str){
+    if (StringUtils.isEmpty(str)) {
+      return false;
+    }
     for(int i=str.length();--i>=0;){
       int chr=str.charAt(i);
-      if(chr<48 || chr>57)
+      if (chr < 48 || chr > 57) {
         return false;
+      }
     }
     return true;
   }
@@ -78,39 +77,40 @@ public class ToLowerInspection extends AbstractBaseJavaLocalInspectionTool {
 
       @Override
       public void visitReferenceExpression(PsiReferenceExpression expression) {
-        String referenceName = expression.getReferenceName();
-        if (isNumeric(referenceName)) {
-          return;
-        }
-        boolean matches = Pattern.matches("(_?[A-Z,0-9]{2,20}){1,6}", referenceName);
-        if (matches) {
-          String lowerText = referenceName.toLowerCase();
-           holder.registerProblem(expression, lowerText, ProblemHighlightType.LIKE_UNUSED_SYMBOL );
-        }
+          String referenceName = expression.getReferenceName();
+          if (isNumeric(referenceName)) {
+              return;
+          }
+          boolean matches = Pattern.matches("(_?[A-Z,0-9]{2,20}){1,6}", referenceName);
+          if (matches && null != referenceName) {
+              String lowerText = referenceName.toLowerCase();
+              holder.registerProblem(expression, "tip: "+lowerText, ProblemHighlightType.LIKE_UNUSED_SYMBOL );
+          }
       }
 
       @Override
       public void visitField(PsiField field) {
-        PsiModifierList modifierList = ((PsiFieldImpl) field).getModifierList();
-        if( !modifierList.hasModifierProperty(PsiModifier.FINAL) ){
-          return;
-        }
-        if( !modifierList.hasModifierProperty(PsiModifier.STATIC)){
-          return;
-        }
+          PsiFieldImpl psiField = (PsiFieldImpl) field;
+          if (null == psiField) {
+              return;
+          }
+          PsiModifierList modifierList = psiField.getModifierList();
 
-        String name = field.getName();
-        if (!StringUtils.isEmpty(name)) {
-          String lowerText = name.toLowerCase();
-          holder.registerProblem(field,
-                  "小写提示: "+lowerText, ProblemHighlightType.LIKE_UNUSED_SYMBOL);
-        }
+          if( !modifierList.hasModifierProperty(PsiModifier.FINAL) ){
+              return;
+          }
+          if( !modifierList.hasModifierProperty(PsiModifier.STATIC)){
+              return;
+          }
 
-        this.visitVariable(field);
+          String name = field.getName();
+          if (!StringUtils.isEmpty(name)) {
+              String lowerText = name.toLowerCase();
+              holder.registerProblem(field,
+                  "tip: "+lowerText, ProblemHighlightType.LIKE_UNKNOWN_SYMBOL);
+          }
+          this.visitVariable(field);
       }
-
     };
   }
-
-  
 }
