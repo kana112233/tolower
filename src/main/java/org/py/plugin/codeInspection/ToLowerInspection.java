1873f71278748a506506fba745744be4868582bd
makeyy22112 <makey221112@gmail.com>
2019-06-24 16:53:56 +0800

    null cast error

--- src/main/java/org/py/plugin/codeInspection/ToLowerInspection.java
+++ src/main/java/org/py/plugin/codeInspection/ToLowerInspection.java
@@ -47,7 +47,7 @@ public class ToLowerInspection extends AbstractBaseJavaLocalInspectionTool {
     return panel;
   }
 
-  private static boolean isNumeric(String str){
+  private static boolean isNumeric(@NotNull String str){
     if (StringUtils.isEmpty(str)) {
       return false;
     }
@@ -78,38 +78,62 @@ public class ToLowerInspection extends AbstractBaseJavaLocalInspectionTool {
       @Override
       public void visitReferenceExpression(PsiReferenceExpression expression) {
           String referenceName = expression.getReferenceName();
-          if (isNumeric(referenceName)) {
+          if (null != referenceName && "".equals(referenceName)) {
               return;
           }
-          boolean matches = Pattern.matches("(_?[A-Z,0-9]{2,20}){1,6}", referenceName);
-          if (matches && null != referenceName) {
+          if(expression.getElement() instanceof PsiEnumConstant ) {
               String lowerText = referenceName.toLowerCase();
               holder.registerProblem(expression, "tip: "+lowerText, ProblemHighlightType.LIKE_UNUSED_SYMBOL );
+          }else{
+              if (isNumeric(referenceName)) {
+                  return;
+              }
+              boolean matches = Pattern.matches("_?[A-Z,0-9]{2,20}(_?[A-Z,0-9]{2,20}){0,6}", referenceName);
+              if (matches) {
+                  String lowerText = referenceName.toLowerCase();
+                  holder.registerProblem(expression, "tip: "+lowerText, ProblemHighlightType.LIKE_UNUSED_SYMBOL );
+              }
           }
+
       }
 
       @Override
       public void visitField(PsiField field) {
-          PsiFieldImpl psiField = (PsiFieldImpl) field;
-          if (null == psiField) {
-              return;
-          }
-          PsiModifierList modifierList = psiField.getModifierList();
+          if(field instanceof PsiEnumConstant){
+//              PsiEnumConstantImpl psiEnumConstant = (PsiEnumConstantImpl) field;
+              String name = field.getName();
+              if (!StringUtils.isEmpty(name)) {
+                  String lowerText = name.toLowerCase();
+                  holder.registerProblem(field,
+                          "tip: "+lowerText, ProblemHighlightType.LIKE_UNKNOWN_SYMBOL);
+              }
+              this.visitVariable(field);
 
-          if( !modifierList.hasModifierProperty(PsiModifier.FINAL) ){
-              return;
-          }
-          if( !modifierList.hasModifierProperty(PsiModifier.STATIC)){
-              return;
+          }else if(field instanceof PsiFieldImpl){
+              PsiFieldImpl psiField = (PsiFieldImpl) field;
+              if (null == psiField) {
+                  return;
+              }
+              PsiModifierList modifierList = psiField.getModifierList();
+
+              if( !modifierList.hasModifierProperty(PsiModifier.FINAL) ){
+                  return;
+              }
+              if( !modifierList.hasModifierProperty(PsiModifier.STATIC)){
+                  return;
+              }
+
+              String name = field.getName();
+              if (!StringUtils.isEmpty(name)) {
+                  String lowerText = name.toLowerCase();
+                  holder.registerProblem(field,
+                          "tip: "+lowerText, ProblemHighlightType.LIKE_UNKNOWN_SYMBOL);
+              }
+              this.visitVariable(field);
+          }else {
+
           }
 
-          String name = field.getName();
-          if (!StringUtils.isEmpty(name)) {
-              String lowerText = name.toLowerCase();
-              holder.registerProblem(field,
-                  "tip: "+lowerText, ProblemHighlightType.LIKE_UNKNOWN_SYMBOL);
-          }
-          this.visitVariable(field);
       }
     };
   }
